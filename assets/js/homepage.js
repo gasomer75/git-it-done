2288e26c05742c3b1660086e00b68a0f6352efb0
gsomer <gasomer75@hotmail.com>
2022-01-27 13:52:02 -0800

    add ability for user to be modified in repo list fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            response.json()
+            .then(function(data){
+            console.log(response);
+        });
+    });
 };
 
 getUserRepos();
